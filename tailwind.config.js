b8055360a810bf2048d8e17e87083bebf18593cf
andyh00 <yoboijz00@gmail.com>
2025-05-31 21:58:04 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+    // adjust paths to match your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
